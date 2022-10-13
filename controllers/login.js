2d57f1274f44b2a9222e238a53ea1aa30b685755
Ben King <benking1293@gmail.com>
2022-10-13 13:14:10 +0100

    Created login controller methods

--- controllers/login.js
+++ controllers/login.js
@@ -1,1 +1,47 @@
 const schemas = require('..models/schemas.js')
+const bcrypt = require('bcrypt')
+
+exports.getLogin = (req,res) => {
+    res.render('login', {title: 'Login', loggedIn: false, error: null})
+}
+
+exports.getSignup = (req,res) => {
+    res.render('new-acct', {title: 'New Account', loggedIn: false, error: null})
+}
+
+exports.getLogout = (req, res) => {
+    req.session.destroy()
+    res.redirect('/')
+}
+
+exports.postLogin = async (req,res) => {
+    let email = req.body.email
+    let password = req.body.password
+    let loginSuccess = false
+    let sesh = req.session
+    sesh.loggedIn = false
+
+    let users = schemas.users
+    let qry = {email: email}
+
+    // replace with password
+    if(email !== '' && password !== '') {
+        let usersResult = await users.findOne(qry)
+        .then( async(data) => {
+            if (data) {
+                let passResult = await bcrypt.compare(password, data.password)
+                .then((isMatch) => {
+                    if (isMatch) {
+                    sesh.loggedIn = true
+                    loginSuccess = true
+                    }
+                })
+            }
+        })
+    }
+    if (loginSucess === true) {
+        res.redirect('/')
+    } else {
+        res.render('login', {title: 'Login', loggedIn: false, error: 'Invalid Login!'})
+    }
+}
