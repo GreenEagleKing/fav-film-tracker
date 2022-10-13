08dec421969747bb89560890dd73ccec79e4ddcc
Ben King <benking1293@gmail.com>
2022-10-13 14:36:10 +0100

    Completed login controller temp methods

--- controllers/login.js
+++ controllers/login.js
@@ -45,3 +45,33 @@ exports.postLogin = async (req,res) => {
         res.render('login', {title: 'Login', loggedIn: false, error: 'Invalid Login!'})
     }
 }
+
+exports.postSignup = async (req,res) => {
+    let email = req.body.emailInput
+    let password = req.body.passwordInput
+
+    if(email !== '' && password !== '') {
+        let users = schemas.users
+        let qry = {email:email}
+
+        let userSearch = await users.findOne(qry)
+        .then(async(data) => {
+            if(!data) {
+                let saltRounds = 10
+                let passSalt = await bcrypt.genSalt(saltRounds, async(err, salt) => {
+                    let passHash = await bcrypt.hash(pass, salt, async(err, hash) => {
+                        let acct = {email:email, pwd: hash, level: 'admin'}
+                        let newUser = new schemas.users(acct)
+                        let saveUser = await newUser.save()
+                    })
+                })
+            }
+        })
+
+    res.render('login', {title: 'Login', loggedIn: false, error: 'Please login with your account'})
+    } else {
+    res.render('new-acct', {title: 'New Account', loggedIn: false, error: 'All fields are required. Please check and try again'})
+    }
+
+
+}
